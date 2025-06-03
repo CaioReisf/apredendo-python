c22e8e9685cd553512363cd13fcc370b3523610d
caio reis <caioreis00p@gmail.com>
2025-06-03 19:42:17 -0300

    up

---
+++ mundo 1/aula 7/desafios.py
@@ -0,0 +1,31 @@
+# EXERCICIO 5 FEITO ########
+
+# n1 = int(input('digite um numero: '))
+# a = n1 + 1
+# b = n1 - 1
+# print('o antecessor do numero {} eh {} e o sucessor eh {}'.format(n1, b, a))
+
+# EXERCICIO 6 FEITO ########
+
+# n1 = int(input('digite um numero: '))
+# d = n1 ** 2
+# t = n1 ** 3
+# r = n1 ** 0.5
+# print('o dobro de {} eh {}, o triplo eh {}, a RQ eh {:.2}'.format(n1, d, t, r))
+
+# EXERCICIO 7 FEITO #######
+
+# n1 = float(input('primeira nota: '))
+# n2 = float(input('outra nota: '))
+# m = (n1 + n2) /2
+# print('sua media e {}'.format(m))
+
+# EXERCICIO 8 FEITO #######
+
+#n1 = float(input('escreva um numero:'))
+#s = n1 * 100
+#print('{} metros, fica {} em cemtimetros'.format(n1, s))
+
+# EXERCICIO 9 
+
+
---
+++ mundo 1/aula 7/ex001.py
@@ -0,0 +1,12 @@
+n1 = int(input('escreva um numero: '))
+n2 = int(input('mais um numero: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+d1 = n1 % n2
+
+print('a soma vale {}'.format(s))
+print('a multiplicao eh {}'.format(m))
+print('a divisao e {}'.format(d))
+print('o resto da divisao eh  {}'.format(d1))
+
