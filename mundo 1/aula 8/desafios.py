e481741280f4e233bdb469fb964cc5735dd9c8b8
caio reis <caioreis00p@gmail.com>
2025-09-03 03:02:46 -0300

    up

---
+++ mundo 1/aula 8/desafios.py
@@ -0,0 +1,46 @@
+# DESAFIO 16 FEITO #######
+
+# import math
+# n = float(input('digite um numero:'))
+# i = math.trunc(n)
+# print(i)
+
+# DESAFIO 17 FEITO #####
+
+# import math 
+# n1 = float(input('1 angulo:'))
+# n2 = float(input('2 angulo:'))
+# h = math.hypot(n1 ,n2)
+# print('{:.2}'.format(h))
+
+# DESAFIO 18 EM ABERTO ------------ FEITO
+
+# import math
+# an =float(input('digite um angulo:'))
+# se = math.sin(math.radians(an))
+# co = math.cos(math.radians(an))
+# tg = math.tan(math.radians(an))
+# print('seno {} coseno {} tangente{}'.format(se,co.tg))
+
+# DESAFIO 19 FEITO ######
+
+# import random
+# lista = 'ana', 'joao', 'carlos', 'marta'
+# e = random.choice(lista)
+# print(e)
+
+# DESAFIO 20 FEITO ---------
+
+# import random
+# n1 = str( 'ana')
+# n2 = str( 'joao')
+# n3 = str( 'carlos')
+# n4 = str( 'marta')
+# lista = [n1, n2, n3, n4]
+# random.shuffle(lista)
+# print(lista)
+
+
+
+
+
---
+++ mundo 1/aula 8/ex001.py
@@ -0,0 +1,5 @@
+from math import sqrt, ceil
+
+num = int(input('digite um numereo:'))
+raiz = sqrt(num)
+print('a raiz de {} eh de {}'.format(num,ceil(raiz)))
