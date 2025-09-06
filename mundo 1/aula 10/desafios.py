679084f8d87da48c977660a57485c02980a5b77d
caio reis <caioreis00p@gmail.com>
2025-09-06 06:30:07 -0300

    up

--- mundo 1/aula 10/desafios.py
+++ mundo 1/aula 10/desafios.py
@@ -8,10 +8,74 @@
 # else:
 #    print('Voce Errou TENTE NOVAMENTE!!')
 
-# DESAFIO 29
+# DESAFIO 29 FEITO #######
 
-km = float(input('quantos quilometros:'))
-if km <= 80:
-    print('OK! dirija com cuidado!')
-else:
-    print('VOCE FOI MULTADO! multa de {}')
+# km = float(input('quantos quilometros:'))
+# if km <= 80:
+#    print('OK! dirija com cuidado!')
+# else:
+#    c = (km - 80) *7
+#    print('VOCE FOI MULTADO! multa de {}'.format(c))
+
+# DESAFIO 30 FEITO ######
+
+# num = int(input('digite um numero:'))
+# if num % 2 == 0:
+#    print('seu numero eh PAR!')
+# else:
+#    print('seu numero eh IMPAR!')
+
+# DESAFIO 31 FEITO ####
+
+# km = float(input('qual a distancia em km:'))
+# if km<=200:
+#    passagem = km*0.50
+#    print('a passagem eh de {}R$'.format(passagem))
+# else:
+#    ps = km*0.45
+#    print('a passagem eh de {}R$'.format(ps))
+
+# DESAFIO 32 FEITO ####
+
+# ano = int(input('digite um ano:'))
+# if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+#    print('{} eh BISSEXTO'.format(ano))
+# else:
+#    print('{} NAO eh bissexto'.format(ano))
+
+# DESAFIO 33 EM ABERTO --------
+
+# n1 = int(input('digite um numero:'))
+# n2 = int(input('digite outro numero:'))
+# n3 = int(input('digite o ultimo numero:'))
+# if (n1 >= n2 and n2 >= n3) or n2
+
+# DESAFIO 34 FEIIO ####
+
+# salario = float(input('digite seu salario:'))
+# if salario >= 1250:
+#    m = 10 /100
+#    a1 = (salario*m)
+#    a2 = salario + a1
+#    print('o novo salario eh de {}'.format(a2))
+# else:
+#    m1 = 15 /100
+#    a3 = (salario*m1)
+#    a4 = salario + a3
+#    print('o novo salario eh de {}'.format(a4))
+
+# DESAFIO 35 FEITO #######
+
+# a = float(input('reta 1:'))
+# b = float(input('reta 2:'))
+# c = float(input('reta 3:'))
+# if a + b > c and a + c > b and b + c > a:
+#    print('essas retas formam um triangulo')
+# else:
+#    print('essas retas NAO formam um triangulo')
+
+
+
+
+
+
