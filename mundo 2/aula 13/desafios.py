bb33baa61b393fc2037783f820607561a60b0c9c
caio reis <caioreis00p@gmail.com>
2025-09-17 09:21:10 -0300

    up

--- mundo 2/aula 13/desafios.py
+++ mundo 2/aula 13/desafios.py
@@ -29,13 +29,13 @@
 #    tab = num * c
 #    print(f'{num} X {c} = {tab}')
 
-# DESAFIO 50
+# DESAFIO 50 FEITO #######
 
-s = 0
-cont = 0
-for c in range(0, 6):
-    num = int(input('digite um numero:'))
-    if (num % 2 == 0):
-        s += num
-        cont = cont + 1
-print(f'a soma dos {cont} numeros pares eh de {s}')
+# s = 0
+# cont = 0
+# for c in range(0, 6):
+#   num = int(input('digite um numero:'))
+#    if (num % 2 == 0):
+#        s += num
+#        cont = cont + 1
+# print(f'a soma dos {cont} numeros pares eh de {s}')
